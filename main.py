847e4c9c213aa19e7facb08780a3153e579f155f
FilipZajdel <filip97.zajdel@gmail.com>
2020-06-21 13:48:30 +0200

    Add notify icon and play sound using external thread

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import time
 from datetime import datetime
 import subprocess
+from threading import Thread
 from playsound import playsound
 import sys
+import json
 
-
-intervals = {
-        "work" : 1500,
-        "break": 300
-        }
-
-sounds = {
-        "ringbell":"sounds/bell_sound.mp3"
-        }
+POMODORO_PATH = "/home/filip/Projects/pomodoro"
+ICON_PATH = f"{POMODORO_PATH}/icons/tomato.png"
 
 def on_work():
-    playsound(sounds["ringbell"])
-    current_time = datetime.now().strftime("%H:%M:%S") 
-    subprocess.call(["notify", f"{current_time} Break has finished, go back to work!"])
+    Thread(target=playsound, args=(sounds["ringbell"], )).start()
+    subprocess.call(["notify-send", 
+        "Pomodoro", "Break has finished, go back to work!",
+        f"--icon={ICON_PATH}"])
+
 
 def on_break():
-    playsound(sounds["ringbell"])
-    current_time = datetime.now().strftime("%H:%M:%S") 
-    subprocess.call(["notify", f"{current_time} Take a break!"])
+    Thread(target=playsound, args=(sounds["ringbell"], )).start()
+    subprocess.call(["notify-send", 
+        "Pomodoro", "Take a break!",
+        f"--icon={ICON_PATH}"])
+
 
 def next_state(state):
     if state == "work":
@@ -30,21 +29,19 @@ def next_state(state):
     else:
         return "work"
 
+
 def timer(description, interval_sec):
     
     sec_elapsed = 0
 
     while sec_elapsed < interval_sec:
-        time.sleep(1)
-        sec_elapsed += 1
+        
         minutes = sec_elapsed//60
         seconds = sec_elapsed%60
-        print(f"\r{description} - {minutes:02d}:{seconds:02d}   ({interval_sec//60}:{(interval_sec%60):02d})", end="")
-
-states = {
-        "work" : on_work,
-        "break": on_break
-        }
+        print("\r" + 80*" "+ f"\r{description} - {minutes:02d}:{seconds:02d}\t"
+            f"({interval_sec//60}:{(interval_sec%60):02d})", end="")
+        time.sleep(1)
+        sec_elapsed += 1
 
 def parse_args():
     if len(sys.argv) == 3:
@@ -55,27 +52,50 @@ def parse_args():
             print("Bad arguments provided")
             quit()
     
-    work_time = intervals["work"]//60
-    break_time = intervals["break"]//60
-    print(f"Using work: {work_time}mins break: {break_time}mins")
+    work_time_m = intervals["work"]//60
+    work_time_s = intervals["work"]%60
+    break_time_m = intervals["break"]//60
+    break_time_s = intervals["break"]%60
+    print(f"Using work: {work_time_m:02d}:{work_time_s:02d} "
+        f"break: {break_time_m:02d}:{break_time_s:02d}")
+
+
+intervals = {
+        "work" : 1500,
+        "break": 300
+        }
+
+
+sounds = {
+        "ringbell":f"{POMODORO_PATH}/sounds/bell_sound.mp3"
+        }
+
+
+states = {
+        "work" : on_work,
+        "break": on_break
+        }
+
 
 if "__main__" == __name__:
 
-
     parse_args()
+    begin_time = datetime.now().strftime("%Y:%m:%d-%H:%M:%S")
     state = "work"
     full_periods = 0
 
     try:
         while True:
             states[state]()
-            # time.sleep(intervals[state]) 
             timer(state ,intervals[state])
             state = next_state(state)
             full_periods += 1
     except KeyboardInterrupt:
-        current_time = datetime.now().strftime("%H:%M:%S")
-        filename = f"{current_time}_stat.log"
+        current_time = datetime.now().strftime("%Y:%m:%d-%H:%M:%S")
+        filename = f"{current_time}.json"
         with open(f"stats/{filename}", "w") as logfile:
-            logfile.write(f"Session ended at {current_time}\nFull periods: {full_periods}")
+            log_data = {"session-start" : begin_time,
+                        "session-finish" : current_time,
+                        "full-periods" : full_periods }
+            json.dump(log_data, logfile)
 
