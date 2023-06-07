0305009bed12163e92a26db08c6b955d23bfdfd7
Filip Zajdel <filip.zajdel@nordicsemi.no>
2023-06-07 12:29:41 +0200

    main: the timer can now be stopped/resumed

--- main.py
+++ main.py
@@ -10,28 +10,33 @@ from collections import OrderedDict
 
 app_path_abs = Path(__file__).resolve().parent
 pomodoro_icon_path = app_path_abs.joinpath("icons/tomato.png")
+sounds_paths = { "ringbell" : str(app_path_abs.joinpath("sounds/bell_sound.mp3")) }
+stats_directory = "stats"
 
+full_periods = 0
 
 def on_work():
-    Thread(target=playsound, args=(sounds["ringbell"], )).start()
+    Thread(target=playsound, args=(sounds_paths["ringbell"], )).start()
     subprocess.call(["notify-send",
         "Pomodoro", "Break has finished, go back to work!",
         f"--icon={pomodoro_icon_path}"])
 
 
 def on_break():
-    Thread(target=playsound, args=(sounds["ringbell"], )).start()
+    global full_periods
+
+    Thread(target=playsound, args=(sounds_paths["ringbell"], )).start()
     subprocess.call(["notify-send",
         "Pomodoro", "Take a break!",
         f"--icon={pomodoro_icon_path}"])
+    full_periods += 1
 
 
 def on_selftest():
-    Thread(target=playsound, args=(sounds["ringbell"], )).start()
+    Thread(target=playsound, args=(sounds_paths["ringbell"], )).start()
     subprocess.call(["notify-send",
         "Pomodoro", "Make an assignment!",
         f"--icon={pomodoro_icon_path}"])
-
 
 def states_sequencer():
     global states
@@ -40,35 +45,49 @@ def states_sequencer():
         yield key, val
         states.move_to_end(key)
 
-def infinite_spinner(direction):
+def spinner():
     components = ("|", "/", "-", "\\")
     idx = 0
 
     while True:
         yield components[idx]
-        if direction == "right":
-            idx = (idx + 1) % len(components)
-        else:
-            idx = (idx - 1) % len(components)
+        idx = (idx + 1) % len(components)
 
-left_spinner = infinite_spinner("left")
-right_spinner = infinite_spinner("right")
+class Timer:
+    spinner_gen = spinner()
 
-def timer(description, interval_sec, period):
+    def __init__(self, description, interval_sec, period):
+        self.desc = description
+        self.interval_sec = interval_sec
+        self.period = period
+        self.completed = False
+        self.sec_elapsed = 0
 
-    sec_elapsed = 0
+    def run(self):
+        if self.completed:
+            self.completed = False
+            self.sec_elapsed = 0
 
-    while sec_elapsed < interval_sec:
+        while self.sec_elapsed < self.interval_sec:
 
-        minutes = int(sec_elapsed)//60
-        seconds = int(sec_elapsed)%60
-        print("\r" + 80*" "+ f"\r🍅 🍅 🍅 ( {next(left_spinner)} ) {description : ^15} "
-              f"{minutes:02d}:{seconds:02d}\t"
-              f"({interval_sec//60}:{(interval_sec%60):02d})", end="")
-        time.sleep(period)
-        sec_elapsed += period
+            minutes = int(self.sec_elapsed)//60
+            seconds = int(self.sec_elapsed)%60
+            print("\r" + 80*" "+ f"\r🍅 🍅 🍅 ( {next(Timer.spinner_gen)} ) {self.desc : ^15} "
+                f"{minutes:02d}:{seconds:02d}\t"
+                f"({self.interval_sec//60}:{(self.interval_sec%60):02d})", end="")
+            time.sleep(self.period)
+            self.sec_elapsed += self.period
 
-def parse_args():
+        self.completed = True
+
+    def reset(self):
+        self.completed = False
+        self.sec_elapsed = 0
+
+def get_current_time_formatted():
+    return datetime.now().strftime("%Y:%m:%d-%H:%M:%S")
+
+def cli():
     global intervals
     if len(sys.argv) == 4:
         try:
@@ -92,16 +111,24 @@ def parse_args():
           f"break -> ({break_time_m:02d}:{break_time_s:02d}) "
           f"selftest -> ({selftest_time_m:02d}:{selftest_time_s:02d})\n\n")
 
+    for state, interval in intervals.items():
+        if interval == 0:
+            del(states[state])
+
+
+def save_stats(start_time: str, end_time: str, stats_file: Path, periods: int):
+
+    with open(stats_file, "w") as logfile:
+        log_data = {"session-start" : start_time,
+                    "session-finish" : end_time,
+                    "full-periods" : periods }
+        json.dump(log_data, logfile)
+
 
 intervals = {
         "work" : 600,
         "break" : 300,
         "selftest" : 450
-        }
-
-
-sounds = {
-        "ringbell" : str(app_path_abs.joinpath("sounds/bell_sound.mp3"))
         }
 
 
@@ -111,22 +138,57 @@ states = OrderedDict({
         "break": on_break
         })
 
+interface_keys = {
+    "n" : "Switch forward to the next sequence.",
+    "c" : "Continue already started sequence.",
+    "r" : "Repeat current sequence.",
+    "s" : "Save stats and exit.",
+    "any key" : "Exit."
+}
+
+exit_prompt = """I hope your work was fruitful. Thanks for using Pomodoro!"""
+
 if "__main__" == __name__:
 
-    parse_args()
-    begin_time = datetime.now().strftime("%Y:%m:%d-%H:%M:%S")
-    full_periods = 0
+    cli()
 
     try:
-        for state, func in states_sequencer():
-            func()
-            timer(state, intervals[state], 0.3)
-            full_periods += 1
+        start_time = get_current_time_formatted()
+        sequencer = states_sequencer()
+        state_active = False
+
+        while True:
+            try:
+                if not state_active:
+                    state, action = next(sequencer)
+                    timer = Timer(state, intervals[state], 0.3)
+                    state_active = True
+
+                action()
+                timer.run()
+                state_active = False
+            except KeyboardInterrupt:
+                prompt = "\n".join([f"{k: ^7} - {desc}" for k, desc in interface_keys.items()])
+                user_input = input(f"\r\nTimer paused!\n{prompt}\nYour choice: ")
+
+                if user_input == "n":
+                    state_active = False
+                elif user_input == "c":
+                    pass
+                elif user_input == "r":
+                    timer.reset()
+                    state_active = True
+                elif user_input == "s":
+                    current_time = get_current_time_formatted()
+                    stats_file_path = Path(f"{stats_directory}/{current_time}.json")
+                    save_stats(start_time, current_time, stats_file_path, full_periods)
+                    break
+                else:
+                    break
+
     except KeyboardInterrupt:
-        current_time = datetime.now().strftime("%Y:%m:%d-%H:%M:%S")
-        filename = f"{current_time}.json"
-        with open(f"stats/{filename}", "w") as logfile:
-            log_data = {"session-start" : begin_time,
-                        "session-finish" : current_time,
-                        "full-periods" : full_periods }
-            json.dump(log_data, logfile)
+        # No specific handler for ctrl-c
+        pass
+
+    finally:
+        print(f"\r{exit_prompt}")
